c09df75128714364becd2ff6d16e0be796ad339c
hai <haithngn@gmail.com>
2021-07-16 22:12:13 +0700

    - support Carthage

---
+++ VideoIO/VideoIO.h
@@ -0,0 +1,18 @@
+//
+//  VideoIO.h
+//  VideoIO
+//
+//  Created by Hai Nguyen on 7/16/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VideoIO.
+FOUNDATION_EXPORT double VideoIOVersionNumber;
+
+//! Project version string for VideoIO.
+FOUNDATION_EXPORT const unsigned char VideoIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoIO/PublicHeader.h>
+
+
---
+++ VideoIO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoIO
+//
+//  Created by Hai Nguyen on 7/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
